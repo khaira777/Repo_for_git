0e572333b561e5d6bfb8610429dfad78bca47dda
Gurkirat Khaira <777gurkirat@gmail.com>
2020-06-18 12:02:37 -0400

    This is my lab.

---
+++ First git project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_git_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person tom = new person("Tom", 40);
+            tom.SaysHello();
+            string tomString = tom.ToString();
+
+            Console.WriteLine(tomString);
+            Console.WriteLine();
+
+
+            person jacky = new person("Jacky", 22);
+
+            jacky.SaysHello();
+
+            Console.WriteLine(jacky.ToString());
+            Console.WriteLine();
+
+            /*Student joss = new Student("Joss", 22, "A1234567890");
+
+            joss.SaysHello();
+            joss.Studies();
+
+            Console.WriteLine(joss.ToString());*/
+            Console.ReadLine();
+        }
+    }
+}
---
+++ First git project/person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_git_project
+{
+    class person
+    {
+        private string m_name;
+        private int m_age;
+        public string Name{
+            get
+            {return m_name;}
+            set
+            {m_name = value;}
+        }
+        public int Age
+        {
+            get
+            {return m_age;}
+            set
+            {m_age = value;}
+        }
+        public person(string name, int age)
+        {
+            m_name = name;
+            m_age = age;
+        }
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello");
+        }
+        public override string ToString()
+        {
+            string outputString = "";
+            outputString += "\n++++++++++++++++++++++++\n";
+            outputString += $" Name: {Name}         \n";
+            outputString += $" Age : {Age}          \n";
+            outputString += "++++++++++++++++++++++++\n";
+            return outputString;
+        }
+    }
+}
